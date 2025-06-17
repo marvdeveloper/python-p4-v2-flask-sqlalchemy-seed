151a8dbf246360f5dc064f5137060ef8d0d518cd
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-06-17 10:59:55 +0300

    Add seed script to populate pets table with sample data

--- server/seed.py
+++ server/seed.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
-#server/seed.py
+# server/seed.py
+
+from random import choice as rc
+from faker import Faker
 
 from app import app
 from models import db, Pet
 
 with app.app_context():
+    # Initialize faker
+    fake = Faker()
 
-    # Create an empty list
+    # Delete all rows from the pets table
+    Pet.query.delete()
+
+    # Define possible species
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+
+    # Create 10 random pets
     pets = []
+    for _ in range(10):
+        pet = Pet(name=fake.first_name(), species=rc(species))
+        pets.append(pet)
 
-    # Add some Pet instances to the list
-    pets.append(Pet(name = "Fido", species = "Dog"))
-    pets.append(Pet(name = "Whiskers", species = "Cat"))
-    pets.append(Pet(name = "Hermie", species = "Hamster"))
-
-    # Insert each Pet in the list into the database table
+    # Insert and commit
     db.session.add_all(pets)
-
-    # Commit the transaction
     db.session.commit()
